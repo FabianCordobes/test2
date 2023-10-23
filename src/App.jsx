eaf1a52065713ea0962a62a2cc92becb081385f8
fabiancord <fabianarielcordobes@gmail.com>
2023-10-23 17:57:41 -0300

    commit#2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-    <h1>Hoola Test</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>Hoola Test</h1>
+			<ul>
+				<li>1</li>
+				<li>twefwev</li>
+				<li>2e</li>
+				<li>12rfaf</li>
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
